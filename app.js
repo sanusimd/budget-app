672c12a34b03541e9ad9a86e9e1d3334b6e299f4
Sanusi Muhyideen <just4deen@yahoo.com>
2022-05-26 13:02:12 +0100

    Create add expense functoinality

---
+++ app.js
@@ -0,0 +1,79 @@
+// Declearation of variable
+const form = document.querySelector("#addForm");
+
+const itemList = document.getElementById("items");
+
+// Add EventListener to add button (submit)
+form.addEventListener("submit", addExpense);
+
+// function for Add Expense
+function addExpense(e) {
+  e.preventDefault();
+
+  // Declear varaible for budget Title
+  const budgetTitle = document.querySelector("#InputBudgetTitle").value;
+
+  // Declear varaible for budget Amount
+  const budgetAmount = document.querySelector("#InputAmount").value;
+
+  // Create li Element
+  const li = document.createElement("li");
+
+  //   Add class to li element
+  li.className = "list-group-item";
+
+  //   append value from budget title (text node) to li
+  li.appendChild(document.createTextNode(budgetTitle));
+
+  //   append li to parent element ul
+
+  itemList.appendChild(li);
+
+  //   Create Element Span
+  const span = document.createElement("span");
+
+  console.log(span);
+
+  //   Add classname to span
+  span.className = "budgetAmount ms-3 me-3";
+
+  //   Append value of Amount budget to span
+  li.appendChild(document.createTextNode(budgetAmount));
+
+  //   Create Update Button element
+  const updateBtn = document.createElement("button");
+  console.log(updateBtn);
+
+  //   Add ClassName to Update Button
+  updateBtn.className = "btn btn-success btn-sm float-right delete ms-3 me-3";
+
+  //   Add text node to update button
+  const updateText = document.createTextNode("Update");
+  console.log(updateText);
+
+  //   Append text to update button
+  updateBtn.appendChild(updateText);
+
+  //   append update button  to li (parentelement)
+  li.appendChild(updateBtn);
+
+  //   Create Update Button element
+  const deleteBtn = document.createElement("button");
+  console.log(deleteBtn);
+
+  //   Add ClassName to Update Button
+  deleteBtn.className = "btn btn-danger btn-sm float-right delete ms-3 me-3";
+
+  //   Add text node to update button
+  const deleteText = document.createTextNode("Delete");
+  console.log(deleteText);
+
+  //   Append text to update button
+  deleteBtn.appendChild(deleteText);
+
+  //   append update button  to li (parentelement)
+  li.appendChild(deleteBtn);
+
+  //   form Reset
+  form.reset();
+}
